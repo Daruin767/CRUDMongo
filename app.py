f42696239d8c84b02b8ad8426f4f233efb4648c1
Daruin767 <daruinmontoya123@.com>
2025-03-26 15:40:15 -0500

    Este código define una aplicación web hecha con Flask que interactúa con una base de datos para gestionar los productos, usuarios y prestamos, este permite agregar, editar, eliminar y visualizar los productos, usuarios y préstamos a través de formularios web. La aplicación utiliza una base de datos (En este caso MongoDB)

--- app.py
+++ app.py
@@ -1,102 +1,134 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-import database as dbase  
-from usuario import User  # Asegúrate de que 'usuario.py' contenga la clase 'User'
-from loan import Loan  # Asegúrate de que 'loan.py' contenga la clase 'Loan'
-from product import Product  # Asegúrate de que 'product.py' contenga la clase 'Product'
-
-db = dbase.dbConnection()
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-# Ruta principal para mostrar productos, usuarios y préstamos
+# Conexión a MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['biblioteca']
+
 @app.route('/')
 def home():
-    products = db['products']
-    users = db['users']
-    loans = db['loans']
-    
-    productsReceived = products.find()
-    usersReceived = users.find()
-    loansReceived = loans.find()
-    
-    return render_template('index.html', products=productsReceived, users=usersReceived, loans=loansReceived)
+    products = db['productos'].find()
+    users = db['usuarios'].find()
+    loans = db['prestamos'].find()
+    return render_template('index.html', products=products, users=users, loans=loans)
 
-# Método POST para agregar un producto
-@app.route('/products', methods=['POST'])
+# Agregar Producto
+@app.route('/productos', methods=['POST'])
 def addProduct():
-    products = db['products']
     name = request.form['name']
     price = request.form['price']
-    quantity = request.form['quantity']
+    cuantity = request.form['cuantity']
 
-    if name and price and quantity:
-        product = Product(name, price, quantity)
-        products.insert_one(product.toDBCollection())  # Asegúrate de que 'toDBCollection' esté definido en 'Product'
-        return redirect(url_for('home'))
-    else:
-        return redirect(url_for('home', message="Error: Todos los campos son requeridos"))
-
-# Método POST para agregar un usuario
-@app.route('/users', methods=['POST'])
-def addUser():
-    users = db['users']
-    user_name = request.form['user_name']
-    user_email = request.form['user_email']
-    user_phone = request.form['user_phone']
-
-    if user_name and user_email and user_phone:
-        user = User(user_name, user_email, user_phone)
-        users.insert_one(user.toDBCollection())  # Asegúrate de que 'toDBCollection' esté definido en 'User'
-        return redirect(url_for('home'))
-    else:
-        return redirect(url_for('home', message="Error: Todos los campos son requeridos"))
-
-# Método POST para registrar un préstamo
-@app.route('/loans', methods=['POST'])
-def addLoan():
-    loans = db['loans']
-    user_name = request.form['user_name']
-    book_name = request.form['book_name']
-    loan_date = request.form['loan_date']
-    return_date = request.form['return_date']
-
-    if user_name and book_name and loan_date and return_date:
-        loan = Loan(user_name, book_name, loan_date, return_date)
-        loans.insert_one(loan.toDBCollection())  # Asegúrate de que 'toDBCollection' esté definido en 'Loan'
-        return redirect(url_for('home'))
-    else:
-        return redirect(url_for('home', message="Error: Todos los campos son requeridos"))
-
-# Método DELETE para eliminar un producto
-@app.route('/delete/<string:product_name>')
-def delete(product_name):
-    products = db['products']
-    products.delete_one({'name' : product_name})
+    if name and price and cuantity:
+        db['productos'].insert_one({
+            'name': name,
+            'price': price,
+            'cuantity': cuantity
+        })
     return redirect(url_for('home'))
 
-# Método PUT para editar un producto
-@app.route('/edit/<string:product_name>', methods=['POST'])
-def edit(product_name):
-    products = db['products']
-    name = request.form['name']
-    price = request.form['price']
-    quantity = request.form['quantity']
+# Editar Producto
+@app.route('/edit-product/<product_id>', methods=['GET', 'POST'])
+def editProduct(product_id):
+    if request.method == 'POST':
+        updated_name = request.form['name']
+        updated_price = request.form['price']
+        updated_cuantity = request.form['cuantity']
 
-    if name and price and quantity:
-        products.update_one({'name' : product_name}, {'$set' : {'name' : name, 'price' : price, 'quantity' : quantity}})
+        db['productos'].update_one(
+            {'_id': ObjectId(product_id)},
+            {'$set': {'name': updated_name, 'price': updated_price, 'cuantity': updated_cuantity}}
+        )
         return redirect(url_for('home'))
-    else:
-        return redirect(url_for('home', message="Error: Todos los campos son requeridos"))
+    
+    product = db['productos'].find_one({'_id': ObjectId(product_id)})
+    return render_template('edit_product.html', product=product)
 
-@app.errorhandler(404)
-def notFound(error=None):
-    message ={
-        'message': 'No encontrado ' + request.url,
-        'status': '404 Not Found'
-    }
-    response = jsonify(message)
-    response.status_code = 404
-    return response
+# Eliminar Producto
+@app.route('/delete-product/<product_id>', methods=['GET'])
+def deleteProduct(product_id):
+    db['productos'].delete_one({'_id': ObjectId(product_id)})
+    return redirect(url_for('home'))
+
+# Agregar Usuario
+@app.route('/usuarios', methods=['POST'])
+def addUser():
+    name = request.form['user-name']
+    email = request.form['user-email']
+    phone = request.form['user-phone']
+
+    if name and email and phone:
+        db['usuarios'].insert_one({
+            'name': name,
+            'email': email,
+            'phone': phone
+        })
+    return redirect(url_for('home'))
+
+# Editar Usuario
+@app.route('/edit-user/<user_id>', methods=['GET', 'POST'])
+def editUser(user_id):
+    if request.method == 'POST':
+        updated_name = request.form['user-name']
+        updated_email = request.form['user-email']
+        updated_phone = request.form['user-phone']
+
+        db['usuarios'].update_one(
+            {'_id': ObjectId(user_id)},
+            {'$set': {'name': updated_name, 'email': updated_email, 'phone': updated_phone}}
+        )
+        return redirect(url_for('home'))
+
+    user = db['usuarios'].find_one({'_id': ObjectId(user_id)})
+    return render_template('edit_user.html', user=user)
+
+# Eliminar Usuario
+@app.route('/delete-user/<user_id>', methods=['GET'])
+def deleteUser(user_id):
+    db['usuarios'].delete_one({'_id': ObjectId(user_id)})
+    return redirect(url_for('home'))
+
+# Agregar Préstamo
+@app.route('/prestamo', methods=['POST'])
+def addLoan():
+    user_name = request.form['user-name']
+    book_name = request.form['book-name']
+    loan_date = request.form['loan-date']
+    return_date = request.form['return-date']
+
+    if user_name and book_name and loan_date and return_date:
+        db['prestamos'].insert_one({
+            'user_name': user_name,
+            'book_name': book_name,
+            'loan_date': loan_date,
+            'return_date': return_date
+        })
+    return redirect(url_for('home'))
+
+# Eliminar Préstamo
+@app.route('/delete-loan/<loan_id>', methods=['GET'])
+def deleteLoan(loan_id):
+    db['prestamos'].delete_one({'_id': ObjectId(loan_id)})
+    return redirect(url_for('home'))
+
+# Editar Préstamo
+@app.route('/edit-loan/<loan_id>', methods=['GET', 'POST'])
+def editLoan(loan_id):
+    if request.method == 'POST':
+        updated_loan_date = request.form['loan-date']
+        updated_return_date = request.form['return-date']
+
+        db['prestamos'].update_one(
+            {'_id': ObjectId(loan_id)},
+            {'$set': {'loan_date': updated_loan_date, 'return_date': updated_return_date}}
+        )
+        return redirect(url_for('home'))
+
+    loan = db['prestamos'].find_one({'_id': ObjectId(loan_id)})
+    return render_template('edit_loan.html', loan=loan)
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
